21ce283cb5d046c3e62708d9060069cf9e26c852
Unknown <naberhausj@gmail.com>
2018-03-24 00:09:29 -0500

    Implemented player movement

--- Public/Javascript/Entities/Player.js
+++ Public/Javascript/Entities/Player.js
@@ -1,13 +1,15 @@
 Player = function(game, initialX, initialY) {
 	Phaser.Sprite.call(this, game, initialX, initialY, 'player');
 
+	this.scale.setTo(1.5, 1.5)
+
 	this.direction = "up";
 	this.moving = false;
 	this.curMoveSpeed = "normal";
 	this.movementSpeeds = {
-		"normal":10,
-		"sprint":15,
-		"crawl":7
+		"normal":4,
+		"sprint":6,
+		"crawl":2
 	}
 	game.add.existing(this);
 
@@ -17,7 +19,20 @@ Player.prototype = Object.create(Phaser.Sprite.prototype);
 Player.prototype.constructor = Player;
 
 Player.prototype.update = function() {
-	
+	switch (this.direction) {
+		case "left":
+			this.x -= this.movementSpeeds[this.curMoveSpeed];
+			break;
+		case "right":
+			this.x += this.movementSpeeds[this.curMoveSpeed];
+			break;
+		case "up":
+			this.y -= this.movementSpeeds[this.curMoveSpeed];
+			break;
+		case "down":
+			this.y += this.movementSpeeds[this.curMoveSpeed];
+			break;
+	} 
 }
 
 Player.prototype.getData = function() {

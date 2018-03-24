16cb2cf970792bd093ee737beb58baa224367801
Unknown <naberhausj@gmail.com>
2018-03-24 06:53:39 -0500

    A whole lot of stuff!

--- Public/Javascript/Entities/Player.js
+++ Public/Javascript/Entities/Player.js
@@ -16,37 +16,63 @@ Player = function(game, playerName, playerId, initialX, initialY) {
 	this.hasChanged = false;
 	game.add.existing(this);
 
+	this.animations.add('stand',[0], 0, true);
+	this.animations.add('walkDown',[0,1], 0, true);
+	this.animations.add('walkUp',[2,3], 0, true);
+	this.animations.add('walkLeft',[6,7], 0, true);
+	this.animations.add('walkRight',[4,5], 0, true);
+
+
 }
 
 Player.prototype = Object.create(Phaser.Sprite.prototype);
 Player.prototype.constructor = Player;
 
 Player.prototype.update = function() {
-	// Do we need to update other clients
-	if (this.moving == true) {
-		this.hasChanged = true;
+	
+	if (this.moving) {
+		switch (this.direction) {
+			case "left":
+				this.x -= this.movementSpeeds[this.curMoveSpeed];
+				this.animations.play('walkLeft',8, false);
+				break;
+			case "right":
+				this.x += this.movementSpeeds[this.curMoveSpeed];
+				this.animations.play('walkRight',8, false);
+				break;
+			case "up":
+				this.y -= this.movementSpeeds[this.curMoveSpeed];
+				this.animations.play('walkUp',8, false);
+				break;
+			case "down":
+				this.y += this.movementSpeeds[this.curMoveSpeed];
+				this.animations.play('walkDown',8, false);
+				break;
+		} 
 	} else {
-		this.hasChanged = false;
+		switch (this.direction) {
+			case "left":
+			this.animations.play('walkLeft', 100, false);
+			this.animations.stop(null, true);
+			this.animations.frame = 6;
+			break;
+			case "right":
+			this.animations.play('walkRight', 100, false);
+			this.animations.stop(null, true);
+			this.animations.frame = 4;
+			break;
+			case "up":
+			this.animations.play('walkUp', 100, false);
+			this.animations.stop(null, true);
+			this.animations.frame = 2;
+			break;
+			case "down":
+			this.animations.play('walkDown', 100, false);
+			this.animations.stop(null, true);
+			this.animations.frame = 0;
+			break;
+		}
 	}
-
-	switch (this.direction) {
-		case "left":
-			this.x -= this.movementSpeeds[this.curMoveSpeed];
-			player.animations.play('walkLeft',8, false);
-			break;
-		case "right":
-			this.x += this.movementSpeeds[this.curMoveSpeed];
-			player.animations.play('walkRight',8, false);
-			break;
-		case "up":
-			this.y -= this.movementSpeeds[this.curMoveSpeed];
-			player.animations.play('walkUp',8, false);
-			break;
-		case "down":
-			this.y += this.movementSpeeds[this.curMoveSpeed];
-			player.animations.play('walkDown',8, false);
-			break;
-	} 
 }
 
 Player.prototype.getData = function() {
--- Public/Javascript/index.js
+++ Public/Javascript/index.js
@@ -4,7 +4,7 @@
 
 
 //init variables here
-var userId;
+var id;
 var myClient;
 var connectedCount = 0;
 var socket;
@@ -17,9 +17,11 @@ function init(){
 	"use strict";
 
 	socketSetup();
+}
+
+function gameSetup() {
 	//call phaser init here
 	game = new Phaser.Game(800, 600, Phaser.CANVAS, 'Medivial Warefare', { preload: preload, create: create, update: update, render: render });
-
 }
 
 function socketSetup(){
@@ -29,79 +31,116 @@ function socketSetup(){
 		reconnection: false
 	});
 
-	socket.on("connReply", function(data){
-		userId = data;
+	socket.on("idAssign", function(data) {
+		console.log("Assigned id: "+ data.id);
+		id = data.id;
+		gameSetup();
 	});
 
-
-	//load new users when they connect
-	socket.on("addUser", function(data){
-
-		connectedCount +=1;
-
-		// Unpack data
-		if (data.playerId !== userId) {
-			addUser(data);
+	socket.on("addUsers", function(userDatas) {
+		console.log("Adding users");
+		console.log(userDatas);
+		for (var i = 0; i < userDatas.length; i++) {
+			console.log("Should i add? " + userDatas[i].player.playerId);
+			if (userDatas[i].player.playerId !== id) {
+				console.log(userDatas[i]);
+				addUser(userDatas[i]);
+			}
 		}
-
-		console.log("A user connected!");
-		console.log("There are " + connectedCount + " user online!");
 	});
 
-	//remove users when they disconnect
-	socket.on("userDisconnect", function(data) {
-		
-		//delete the user here/tell server to save his info
-
-		console.log("user disconnected");
-
-		connectedCount -= 1;
-
+	socket.on("updateUser", function(userData) {
+		//console.log("updating with data: " + userData);
 		for (var i = 0; i < otherPlayers.length; i++) {
-			if (otherPlayers[i].playerId == data.id) {
+			if (userData.id == otherPlayers[i].playerId) {
+				otherPlayers[i].unpackData(userData.player);
+			}
+		}
+	});
+
+	socket.on("removeUser", function(data) {
+		for (var i = 0; i < otherPlayers.length; i++) {
+			if (data.id == otherPlayers[i].playerId) {
+				console.log("Removing player with id: " + data.id);
+				otherPlayers[i].destroy();
 				otherPlayers.splice(i, 1);
 			}
 		}
-
-		console.log("There are " + connectedCount + " user online!");
-
 	});
 
-	socket.on("recieveUsers", function(users) {
-		for (let user of users) {
-			if (user != null) {
-				addUser(user);
-			}
-		}
-	});
+	// socket.on("connReply", function(data){
+	// 	userId = data;
+	// 	gameSetup();
+	// });
 
-	socket.on("clientRecievePlayerData", function(data) {
 
-		if(data.clientId !== userId){
-			for (let otherPlayer of otherPlayers) {
-				if (otherPlayer.playerId == data.player.playerId) {
-					otherPlayer.unpackData(data);
-				}
-			}
-		}
+	// //load new users when they connect
+	// socket.on("addUser", function(data){
+
+	// 	connectedCount +=1;
+
+	// 	// Unpack data
+	// 	if (data.player.playerId !== userId) {
+	// 		addUser(data);
+	// 	}
+
+	// 	console.log("A user connected!");
+	// 	console.log("There are " + connectedCount + " user online!");
+	// });
+
+	// //remove users when they disconnect
+	// socket.on("userDisconnect", function(data) {
 		
-	});
+	// 	//delete the user here/tell server to save his info
+	// 	connectedCount -= 1;
 
-	socket.on("disconnect", function(){
+	// 	for (var i = 0; i < otherPlayers.length; i++) {
+	// 		if (otherPlayers[i].playerId === data.id) {
+	// 			console.log("Removing " + i);
+	// 			otherPlayers.split(i,1);
+	// 		}
+	// 	}
 
-		console.log("connection Lost... reload your page");
+	// 	console.log("There are " + connectedCount + " user online!");
 
-	});
+	// });
+
+	// socket.on("recieveUsers", function(users) {
+	// 	for (let user of users) {
+	// 		if (user !== null) {
+	// 			addUser(user);
+	// 		}
+	// 	}
+	// });
+
+	// socket.on("clientRecievePlayerData", function(data) {
+
+	// 	if(data.clientId !== userId){
+	// 		for (let otherPlayer of otherPlayers) {
+	// 			if (otherPlayer.playerId == data.player.playerId) {
+	// 				otherPlayer.unpackData(data);
+	// 			}
+	// 		}
+	// 	}
+		
+	// });
+
+	// socket.on("disconnect", function(){
+
+	// 	console.log("connection Lost... reload your page");
+
+	// });
 
 }
 
 function joinGame() {
 	// Add self to game
-	socket.emit("getUsers");
-	socket.emit("addUser", {"player" : player.getData()});
+	console.log("Adding self to game with id: " + id);
+	socket.emit("addUser", {"id" : id, "player" : player.getData()});
 }
 
 function addUser(userData) {
+	console.log("Adding player with id: " + userData.player.playerId);
 	var newPlayer = new Player(game, userData.player.playerName, userData.player.playerId, userData.player.x, userData.player.y);
 	newPlayer.unpackData(userData.player);
 	otherPlayers.push(newPlayer);

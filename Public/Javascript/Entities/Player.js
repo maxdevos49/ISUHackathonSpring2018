c43b8ce69df152ae1d5fc53a9b5d3aeb4c2c61ae
Unknown <naberhausj@gmail.com>
2018-03-23 22:48:01 -0500

    Player updatae
    pls like :)

--- Public/Javascript/Entities/Player.js
+++ Public/Javascript/Entities/Player.js
@@ -1,26 +1,43 @@
-// Player = function(game, initialX, initialY) {
+class Player {
+	constructor(game, initialX, initialY) {
+		Phaser.Sprite.call(this, game, initialX, initialY);
 
-// 	Phaser.Sprite.call(this, game, initialX, initialY);
+		this.anchor.setTo(0.5, 0.5);
 
-// 	this.anchor.setTo(0.5, 0.5);
+		this.angle = 0;
+		this.moving = false;
+		this.curMoveSpeed = "normal";
+		this.movementSpeeds = {
+			"normal":10,
+			"sprint":15,
+			"crawl":7
+		}
 
-// 	this.angle = 0;
-// 	this.moving = false;
-// 	this.curMoveSpeed = "normal";
-// 	this.movementSpeeds = {
-// 		"normal":10,
-// 		"sprint":15,
-// 		"crawl":7
-// 	}
+		game.add.existing(this);
+		this.prototype = Object.create(Phaser.Sprite.prototype);
+	}
 
-// 	game.add.existing(this);
+	update() {
+		this.x = Math.cos(this.angle) * this.movementSpeeds[this.curMoveSpeed];
+		this.y = Math.sin(this.angle) * this.movementSpeeds[this.curMoveSpeed];
+	}
 
-// }
+	getData() {
+		return {
+			"x" : this.x,
+			"y" : this.y,
+			"angle" : this.angle,
+			"moving" : moving,
+			"curMoveSpeed" : curMoveSpeed
+		}
+	}
 
-// Player.prototype = Object.create(Phaser.Sprite.prototype);
-// Player.prototype.constructor = Player;
+	unpackData(data) {
+		this.x = data.x;
+		this.y = data.y;
+		this.angle = data.angle;
+		this.moving = data.moving;
+		this.curMoveSpeed = data.curMoveSpeed;
+	}
 
-Player.prototype.update = function() {
-	this.x = Math.cos(this.angle) * this.movementSpeeds[this.curMoveSpeed];
-	this.y = Math.sin(this.angle) * this.movementSpeeds[this.curMoveSpeed];
 }

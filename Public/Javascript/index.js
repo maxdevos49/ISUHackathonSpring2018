5f1124f3dcf1f806d220aa082d5ae1bef296aeff
Maxwell DeVos <maxdevos49@gmail.com>
2018-03-23 21:33:58 -0500

    added index.js code

--- Public/Javascript/index.js
+++ Public/Javascript/index.js
@@ -1,11 +1,102 @@
 //linter settings
-//global console:false
+/*global console:false, io:false*/
 //jshint unused:false
 
-//init variables here
 
+//init variables here
+var user = [];
+var myClient;
+var connectedCount = 0;
+var connection = false;
+var socket;
+var username;
+var password;
+var message;
+var posted = "";
+var messageCount = 0;
+var classType;
 
 //initial game start here called by html body
 function init(){
+	"use strict";
+
+	//call socket stuff for start
+	socketSetup();
+
+	//call phaser init here
+
+}
+
+function socketSetup(){
+	"use strict";
+
+		socket = io.connect({
+		reconnection: false
+	});
+
+	socket.on("connReply", function(data){
+
+		myClient = data.clientNum;
+
+		//user[myClient] = new userInfo(data.clientID, data.clientNum, username);
+		//add new user class here
+
+
+
+		//emit a request for all of the other users info
+		socket.emit("loadOtherUsers", user[myClient]);
+	});
+
+
+	//load new users when they connect
+	socket.on("loadOtherUsers", function(data){
+
+		connectedCount = 0;
+
+		for(var i = 0; i < data.length; i++){
+
+			if (data[i] !== null){
+
+				connectedCount +=1;
+
+				if(data[i].clientNum !== myClient){
+
+					console.log("Loading user with index "+data[i].clientNum);
+
+					//user[data[i].clientNum] = new userInfo(data[i].clientID, data[i].clientNum, data[i].clientUserName);
+	                
+
+	                console.log(data[i].clientUserName + " is online!");
+					connection = true;
+				}
+
+			}
+
+		}
+
+		console.log("There are " + connectedCount + " user online!");
+	});
+
+	//remove users when they disconnect
+	socket.on("userDisconnect", function(data){
+		delete user[data.id];
+
+		console.log("user disconnected with index: " + data.id);
+
+		connectedCount -= 1;
+
+		console.log("There are " + connectedCount + " user online!");
+
+	});
+
+	socket.on("disconnect", function(){
+
+		connection = false;
+
+		console.log("connection Lost... reload your page");
+
+	});
+
+
 
 }

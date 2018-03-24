6d77d55522cb9dee2f2be5324b19db03dcfcfc69
Maxwell DeVos <maxdevos49@gmail.com>
2018-03-23 23:55:03 -0500

    Added more connection messages for debugging on client side

--- Public/Javascript/index.js
+++ Public/Javascript/index.js
@@ -9,14 +9,7 @@ var myClient;
 var connectedCount = 0;
 var connection = false;
 var socket;
-// var username;
-// var password;
-// var message;
-// var posted = "";
-// var messageCount = 0;
-// var classType;
 
-console.log("Loaded this");
 
 var game;
 var cursors;
@@ -24,7 +17,6 @@ var cursors;
 //initial game start here called by html body
 function init(){
 	"use strict";
-	console.log("Loaded that");
 
 	//call socket stuff for start
 	socketSetup();
@@ -42,56 +34,33 @@ function socketSetup(){
 
 	socket.on("connReply", function(data){
 
-		//myClient = data.clientNum;
-
-		//user[myClient] = new userInfo(data.clientID, data.clientNum, username);
-		//add new user class here
-
-
-
 		//emit a request for all of the other users info
 		socket.emit("loadOtherUsers", user[myClient]);
+
 	});
 
 
 	//load new users when they connect
 	socket.on("loadOtherUsers", function(data){
 
-		connectedCount = 0;
+		connectedCount +=1;
 
-		for(var i = 0; i < data.length; i++){
+		connection = true;
 
-			//if (data[i] !== null){
-
-				connectedCount +=1;
-
-			//	if(data[i].clientNum !== myClient){
-
-			//		console.log("Loading user with index "+data[i].clientNum);
-
-					//user[data[i].clientNum] = new userInfo(data[i].clientID, data[i].clientNum, data[i].clientUserName);
-	                
-
-	        //        console.log(data[i].clientUserName + " is online!");
-					connection = true;
-			//	}
-
-			//}
-
-		}
-
+		console.log("A user connected!");
 		console.log("There are " + connectedCount + " user online!");
 	});
 
 	//remove users when they disconnect
 	socket.on("userDisconnect", function(data){
-		//delete user[data.id];
+		
+		//delete the user here/tell server to save his info
 
-		//console.log("user disconnected with index: " + data.id);
+		console.log("user disconnected");
 
 		connectedCount -= 1;
 
-		//console.log("There are " + connectedCount + " user online!");
+		console.log("There are " + connectedCount + " user online!");
 
 	});
 
--- Public/Javascript/phaserCreate.js
+++ Public/Javascript/phaserCreate.js
@@ -1,7 +1,9 @@
+//jshint unused:false
+/*global Player:false*/
 var player;
 
 function create() {
-
+	"use strict";
 	player = new Player(game, 0, 0);
 	cursors = game.input.keyboard.createCursorKeys();
 

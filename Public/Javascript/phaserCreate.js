f6067871b3e141dcc0ace4a3e5ce6c07fe95cae2
Unknown <naberhausj@gmail.com>
2018-03-24 07:21:22 -0500

    Small Multiplayer changes

--- Public/Javascript/phaserCreate.js
+++ Public/Javascript/phaserCreate.js
@@ -11,13 +11,8 @@ var cursors;
 
 function create() {
 	"use strict";
-	player = new Player(game, "default", id, 0, 0);
+	player = new Player(game, true, "default", id, 0, 0);
 
-	stand = player.animations.add('stand',[0], 0, true);
-	walkDown = player.animations.add('walkDown',[0,1], 0, true);
-	walkUp = player.animations.add('walkUp',[2,3], 0, true);
-	walkLeft = player.animations.add('walkLeft',[6,7], 0, true);
-	walkRight = player.animations.add('walkRight',[4,5], 0, true);
 	//player.animations.play('walk', [0],0, true);
 
 	//game.physics.startSystem(Phaser.Physics.BOX2D);

450af8cfe2022d4c5435b8b408b6301b2125b88f
Unknown <naberhausj@gmail.com>
2018-03-24 14:49:11 -0500

    Removed hitboxes

--- Public/Javascript/phaserRender.js
+++ Public/Javascript/phaserRender.js
@@ -1,9 +1,3 @@
 function render() {
 	game.debug.inputInfo(16,16);
-	game.debug.body(player);
-	game.debug.body(player.sword);
-	for (var i = 0; otherPlayers.length; i++) {
-		game.debug.body(otherPlayers[i]);
-	}
-
 }
